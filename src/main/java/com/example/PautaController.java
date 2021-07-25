766f91303b5ef13161345493b4eaa7d0f546b6c4
Leonardo Sartori <leonardogt4@hotmail.com>
2021-07-24 21:19:21 -0300

    teste 4

--- src/main/java/com/example/PautaController.java
+++ src/main/java/com/example/PautaController.java
@@ -1,11 +1,13 @@
 package com.example;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping({"/pauta"})
 public class PautaController {
+    @GetMapping
     public String hello(){
         return "Hello";
     }
