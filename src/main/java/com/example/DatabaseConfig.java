02e9a57192e23c54a33294b8119abf7eb080298b
Leonardo Sartori <leonardogt4@hotmail.com>
2021-07-25 00:21:18 -0300

    teste configuracao database 9

--- src/main/java/com/example/DatabaseConfig.java
+++ src/main/java/com/example/DatabaseConfig.java
@@ -2,15 +2,21 @@ package com.example;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -19,14 +25,46 @@ import java.util.ArrayList;
 import java.util.Map;
 
 @Configuration
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@Component
 public class DatabaseConfig {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(DatabaseConfig.class);
+
+    @Bean()
+//@Primary this annotation to be used if more than one DB Config was used.  In that case,
+// using @Primary would give precedence to a the particular "primary" config class
+    @Profile("heroku")
+    public DataSource dataSource(
+            @Value("${spring.datasource.driverClassName}") final String driverClass,
+            @Value("${spring.datasource.url}") final String jdbcUrl,
+            @Value("${spring.datasource.username}") final String username,
+            @Value("${spring.datasource.password}") final String password
+    ) throws URISyntaxException {
+
+
+        return DataSourceBuilder
+                .create()
+                .username(username)
+                .password(password)
+                .url(dbUrl)
+                .driverClassName(driverClass)
+                .build();
+    }
+
+/*
     @Value("${spring.datasource.url}")
     private String dbUrl;
 
     @Autowired
     private DataSource dataSource;
 
+ */
+
+
+/*
     @Primary
     @Bean(name="oneOfManyDataSources")
     public DataSource dataSource() throws SQLException {
@@ -38,6 +76,8 @@ public class DatabaseConfig {
             return new HikariDataSource(config);
         }
     }
+
+ */
 /*
     @Bean
     public String db(Map<String, Object> model) {
