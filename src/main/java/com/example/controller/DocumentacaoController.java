bf4b2d696263654ee255aa5ab7de3b1e320858a0
Leonardo Sartori <leonardogt4@hotmail.com>
2021-07-28 00:48:40 -0300

    add swagger 3

--- src/main/java/com/example/controller/DocumentacaoController.java
+++ src/main/java/com/example/controller/DocumentacaoController.java
@@ -1,12 +1,14 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @ApiIgnore
+@RestController
 public class DocumentacaoController {
     @GetMapping("/")
     public void index(HttpServletResponse response) throws IOException {
